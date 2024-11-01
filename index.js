01a05b10dd8618758042d03cde660876d353c127
jackbland3k <jbb43@njit.edu>
2024-11-01 15:16:15 -0400

    Intial

--- index.js
+++ index.js
@@ -25,33 +25,3 @@ function totalBill(){
     }
     document.querySelector("#NT").innerHTML = "Total bill including tip: $" + NT.toFixed(2);
 }
-
-
-function Reverse(){
-    var input = document.querySelector("#UserInput");
-    var str = input.value;
-    var RS = "";
-    for(var i = str.length -1; i >= 0; i--){
-        RS += str[i];
-    }
-    document.querySelector("#NS").innerHTML = RS;
-}
-
-function palindrome(){
-    var input = document.querySelector("#UserInput");
-    var I = input.value;
-    var RI = "";
-
-    for(var i = I.length -1; i >= 0; i--){
-        RI += I[i];
-    }
-    var Result;
-    if( I === RI){
-    Result = "This integer is a palindrome!";
-    }
-    else{
-    Result = "This integer is not a palindrome.";
-    }
-
-    document.querySelector("#NI").innerHTML = Result;
-}
